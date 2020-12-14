96ee19e3f97c4c88efe71eebb4934d11e65cc785
Bash <ahamedb1991@gmail.com>
2020-12-14 16:14:44 -0500

    Add Express.js

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const http = require('http');
-const routes = require('./routes');
 
-const server = http.createServer(routes.handler);
-console.log(routes.someText);
+const express = require('express');
+
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next(); // allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from express</h1>');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
-
