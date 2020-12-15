7aa335ac98c5bfcf67d4f1659378a13d11a4c928
Bash <ahamedb1991@gmail.com>
2020-12-14 22:41:09 -0500

    add stylesheet

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const path = require('path');
+
+const rootDir = require('./util/path');
+
 const app = express();
 
-app.use(bodyParser.urlencoded());
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product',(req, res, next) => {
-    res.send('<form action = "/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/',(req, res, next) => {
-    res.send('<h1>Hello from express</h1>');
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
